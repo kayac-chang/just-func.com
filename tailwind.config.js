bde13f9bfd74ead2a4f6a0e83493277b7aac5c4a
kayac-chang <egg734631@gmail.com>
2023-10-23 11:10:04 +0800

    feat: 🎸 update blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,18 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '',
+            },
+            'code::after': {
+              content: '',
+            }
+          }
+        }
+      }
     },
   },
   plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],

dc4237098af36da19b52298382b722aa3fcec275
kayac-chang <egg734631@gmail.com>
2023-11-26 16:26:21 +0800

    feat: 🎸 work in singapore

--- app/routes/blogs.$slug.tsx
+++ app/routes/blogs.$slug.tsx
@@ -1,19 +1,30 @@
-import { type LoaderFunctionArgs } from "@remix-run/node";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { assert } from "@sindresorhus/is";
 import parseBlogServer from "~/services/parse-blog.server";
 import getBlogBySlugServer from "~/services/get-blog-by-slug.server";
+import parseFrontmatter from "~/services/parse-frontmatter.server";
+
+export const meta: MetaFunction<typeof loader> = (args) =>
+  args.data?.frontmatter.meta ?? [];
 
 export async function loader(args: LoaderFunctionArgs) {
   assert.string(args.params.slug);
-  return getBlogBySlugServer(args.params.slug).then(parseBlogServer);
+  return getBlogBySlugServer(args.params.slug).then((content) =>
+    Promise.all([parseFrontmatter(content), parseBlogServer(content)]).then(
+      ([frontmatter, body]) => ({
+        frontmatter,
+        body,
+      })
+    )
+  );
 }
 
 export default function Route() {
   const data = useLoaderData<typeof loader>();
   return (
     <main>
-      <article dangerouslySetInnerHTML={{ __html: data }}></article>
+      <article dangerouslySetInnerHTML={{ __html: data.body }}></article>
     </main>
   );
 }

84844c2ef2ddb730c6ff7cab9b27e1d097221d3a
kayac-chang <egg734631@gmail.com>
2024-01-17 17:02:21 +0800

    feat: 🎸 add auto link

--- app/services/parse-blog.server.ts
+++ app/services/parse-blog.server.ts
@@ -1,9 +1,12 @@
 import remarkGfm from "remark-gfm";
 import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
 import { bundleMDX } from "mdx-bundler";
 import remarkMath from "remark-math";
+import remarkBreaks from "remark-breaks";
 import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
 
 const parseBlog = async (source: string) =>
   bundleMDX({
@@ -11,12 +14,23 @@ const parseBlog = async (source: string) =>
     mdxOptions(options) {
       options.remarkPlugins = [
         ...(options.remarkPlugins ?? []),
+        remarkBreaks,
         remarkGfm,
         remarkMath,
       ];
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
         rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "append",
+            content: fromHtmlIsomorphic(
+              '<svg class="octicon octicon-link w-4 h-4" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill="currentColor" d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg>',
+              { fragment: true }
+            ).children,
+          },
+        ],
         rehypePrettyCode,
         rehypeKatex,
       ];
